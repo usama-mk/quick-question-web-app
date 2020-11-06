40fe42067b8d7617c7a15b9ebf597904864a0a56
Usama Muhammad <usamamuhammad45678@gmail.com>
2020-11-06 14:55:53 +0500

    Open/create Room successfull

--- src/Components/SelectRoom.jsx
+++ src/Components/SelectRoom.jsx
@@ -1,6 +1,7 @@
 import { Button, TextField } from '@material-ui/core';
 import React, { useState } from 'react'
 import { withRouter } from 'react-router-dom';
+import { db } from '../firebase';
 
  function SelectRoom(props) {
     const { history } = props;
@@ -38,6 +39,24 @@ const [Emailerror, setEmailError] = useState("");
 // },[roomId]);
 
 const openRoom=(pageURL)=>{
+    if (roomName) {
+        //some clever DB stuff
+        db.collection('rooms').add({
+            roomName: roomName,
+        })
+    }
+    db.collection("rooms").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            if(doc.data().roomName==roomName){
+        //    setRoomID(doc.id);  don't set it instantly and causes problems
+           alert(`your room id is:${doc.id}`)
+           pageURL= `${pageURL}/${doc.id}`;
+           history.push(pageURL);
+                       }
+        });
+    });
+   
 
 }
 
@@ -66,7 +85,7 @@ history.push(pageURL);
                         onChange={handleRoomID}
                     />
                     <div style={{display:"flex", justifyContent:"flex-end", marginTop:"5px"}}>
-                        <Button style={{color:"red", backgroundColor:"#f8bc5d"}} onClick={()=>{joinRoom("/room")}} >Open Room</Button>
+                        <Button style={{color:"red", backgroundColor:"#f8bc5d"}} onClick={()=>{joinRoom("/room")}} >Join Room</Button>
                     </div>
                     <p className="errorMsg">{Emailerror}</p>
                     <h3 style={{textAlign:"center", color:"#4b5a6c"}} >OR:</h3>
@@ -85,7 +104,7 @@ history.push(pageURL);
                         onChange={handleRoomName}
                     />
                     <div style={{display:"flex", justifyContent:"flex-end", marginTop:"5px"}}>
-                        <Button style={{color:"red", backgroundColor:"#f8bc5d"}} onClick={()=>{openRoom()}}>Join Room</Button>
+                        <Button style={{color:"red", backgroundColor:"#f8bc5d"}} onClick={()=>{openRoom("/room")}}>Open Room</Button>
                     </div>
                     
             </div>
